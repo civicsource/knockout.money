834c36ece270b5571d37499cb7c2e5d74645e85c
Mark D'Arensbourg <mdarens@gmail.com>
2016-06-29 14:37:38 -0500

    make consumable via npm

--- knockout.money.js
+++ knockout.money.js
@@ -1,41 +1,34 @@
-(function(root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(["knockout", "accounting"], factory);
-	} else {
-		// Browser globals
-		factory(ko, accounting);
-	}
-}(this, function (ko, acct) {
-	ko.extenders.money = function (target, option) {
-		var result = ko.computed({
-			read: target, //always return the original observable's value
-			write: function (value) {
-				//force to a valid decimal
-				var decValue = parseFloat(value);
-				if (isNaN(decValue)) {
-					target(0);
-				} else {
-					target(decValue);
-				}
+var ko = require("knockout");
+var acct = require("accounting");
+
+ko.extenders.money = function (target) {
+	var result = ko.computed({
+		read: target, //always return the original observable's value
+		write: function (value) {
+			//force to a valid decimal
+			var decValue = parseFloat(value);
+			if (isNaN(decValue)) {
+				target(0);
+			} else {
+				target(decValue);
 			}
-		});
+		}
+	});
 
-		result.formatted = ko.computed({
-			read: function () {
-				var amount = target();
-				if (!amount) {
-					return "$0.00";
-				}
-
-				return acct.formatMoney(amount);
-			},
-			write: function (value) {
-				value = ("" + value).trim();
-				target(acct.unformat(value));
+	result.formatted = ko.computed({
+		read: function () {
+			var amount = target();
+			if (!amount) {
+				return "$0.00";
 			}
-		});
 
-		return result;
-	};
-}));
+			return acct.formatMoney(amount);
+		},
+		write: function (value) {
+			value = ("" + value).trim();
+			target(acct.unformat(value));
+		}
+	});
+
+	return result;
+};
